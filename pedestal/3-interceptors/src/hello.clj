e856553a9175f86ba5baa6a4b0341e2536dcf9ca
Allan Domingues <49369833+asdomingues@users.noreply.github.com>
2021-08-07 12:40:13 -0300

    use interceptors for http content negotiation

--- pedestal/3-interceptors/src/hello.clj
+++ pedestal/3-interceptors/src/hello.clj
@@ -1,13 +1,31 @@
 (ns hello
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+  (:require [clojure.data.json :as json]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]))
 
 (def unmentionables
   #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操" "Saci"})
 
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html"        body
+    "text/plain"       body
+    "application/edn"  (pr-str body)
+    "application/json" (json/write-str body)))
+
 (defn ok [body]
   (when-not (nil? body)
-    {:status 200 :body body}))
+    {:status  200 :body    body}))
 
 (defn bad-request [body]
   {:status 400 :body body})
@@ -28,9 +46,28 @@
                (bad-request "Zero-length name used as parameter\n"))]
     (or resp (not-found))))
 
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %)))})
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))
+       (update-in [:response] coerce-to (accepted-type context))))})
+
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+     ["/echo"  :get echo]}))
 
 (def service-map
   {::http/routes routes
@@ -45,8 +82,7 @@
 (defn start-dev []
   (reset! server
           (http/start (http/create-server
-                       (assoc service-map
-                              ::http/join? false)))))
+                       (assoc service-map ::http/join? false)))))
 
 (defn stop-dev []
   (http/stop @server))
