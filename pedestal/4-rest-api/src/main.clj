e32f6d3c403f697053380e38d990437338afe377
Allan Domingues <49369833+asdomingues@users.noreply.github.com>
2021-08-07 18:02:00 -0300

    create DB functions for REST API tutorial

--- pedestal/4-rest-api/src/main.clj
+++ pedestal/4-rest-api/src/main.clj
@@ -20,7 +20,22 @@
            db-id    (str (gensym "l"))]
        (assoc context :tx-data [assoc db-id new-list])))})
 
+;;;
+;;; "Database" functions
+;;;
 (defonce database (atom {}))
+
+(defn find-list-by-id [dbval db-id]
+  (get dbval db-id))
+
+(defn find-list-item-by-ids [dbval list-id item-id]
+  (get-in dbval [list-id :items item-id] nil))
+
+(defn list-item-add
+  [dbval list-id item-id new-item]
+  (if (contains? dbval list-id)
+    (assoc-in dbval [list-id :items item-id] new-item)
+    dbval))
 
 (def db-interceptor
   {:name :database-interceptor
